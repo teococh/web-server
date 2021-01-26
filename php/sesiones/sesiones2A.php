640a7f29a197e8c23a55cf8498970784cb348131
Teo <55553715+teococh@users.noreply.github.com>
2021-01-26 19:53:26 +0100

    php

---
+++ php/sesiones/sesiones2A.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teo Coch</title>
+</head>
+<body>
+    <article>
+        <form action="post">
+            Nombre:
+            Apellidos:
+            Edad:
+            Peso: <!-- Entre 10 y 150 -->
+            Sexo:
+            Estado civil: <!-- (Soltero, Casado, Viudo, Divorciado, Otro) -->
+            Aficiones: <!-- Cine, Deporte, Literatura, Música, Cómics,
+Series, Videojuegos -->
+            <input type="button" value="Enviar">
+            <input type="reset" value="Borrar">
+        </form>
+    </article>
+</body>
+</html>
