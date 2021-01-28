de7efa63f5abd0dbc753e05e8536f503cc34a7b7
Teo <55553715+teococh@users.noreply.github.com>
2021-01-28 18:41:44 +0100

    commit

--- cliente/ajax/juego-provincias/js/opcion2.js
+++ cliente/ajax/juego-provincias/js/opcion2.js
@@ -1,22 +1,45 @@
 var a;
+let div;
+let json;
 
 window.onload = function () {
 
-    a = Array();
-    for (let index = 0; index < 51; index++) {
-        
-        a.push(index);
+    // inicializar variables
+    div = document.getElementById("contenedor");
 
+    // a es un array con numeros del 0 al 15
+    a = Array();
+    for (let index = 1; index < 17; index++) {
+        a.push(index);
     }
 
-}
-
-function pintarDiv (){
-
-}
-
-function datos(x) {
-    fetch("./js/provincias.json")
+    fetch("js/provincias.json")
     .then(respuesta => respuesta.json() )
-    .then(respuesta => console.log(respuesta["comunidades"][x]["nombre"]))
+    .then(data => {
+        json = data
+        console.log(json)
+        for (let index = 0; index < 16; index++) {
+            let rnd = Math.floor(Math.random() * a.length);
+            pintarDiv(rnd);
+            a.slice(rnd);
+        }
+    });
+    
+    
+    // hago una eleccion de numeros random usando a para no repetir valores
+    
+
 }
+
+function pintarDiv (x){
+
+    let newDiv = document.createElement("div");
+    let bandera = document.createElement("img");
+    bandera.setAttribute("src", json.comunidades[x].bandera);
+    newDiv.classList.add("bandera");
+    newDiv.innerHTML = json.comunidades[x].nombre;
+    newDiv.appendChild(bandera);
+    div.appendChild(newDiv);
+}
+
+
